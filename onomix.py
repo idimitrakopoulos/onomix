08e414ad139ed588e28495188818bc2c87cc0a2d
idimitrakopoulos <idimitrakopoulos@gmail.com>
2015-10-05 10:44:18 +0300

    Multiprocess whois with 10 threads

--- onomix.py
+++ onomix.py
@@ -1,27 +1,37 @@
 import os
-from multiprocessing import Process
+from multiprocessing import Pool
 from string import ascii_lowercase
+import itertools
 
 import whois
 
 import util.opt_parser as parser
 from util.toolkit import log, PermutationIterator
 
+if __name__ == '__main__':
+    def calculate_permutations(character_list, length):
+        d = []
+        for p in itertools.permutations(character_list, length):
+            d.append(''.join(p))
+        return len(d)
 
-def run_name(name):
-    try:
-        whois.query(name)
-        log.info("[" + str(os.getpid()) + "] " + name + " [TAKEN]")
-    except (Exception):
-        log.info("[" + str(os.getpid()) + "] " + name + " [FREE]")
+
+    def run_name(domain_name):
+        try:
+            q = whois.query(domain_name)
+            print q
+            log.info("[" + str(os.getpid()) + "] " + domain_name + " [TAKEN]")
+        except (Exception):
+            log.info("[" + str(os.getpid()) + "] " + domain_name + " [FREE]")
 
 
-for e in PermutationIterator(ascii_lowercase, int(parser.options.length)):
-    name = "".join(e) + parser.options.tld
-    result = None
+    log.info("Permutations are " + str(calculate_permutations(ascii_lowercase, int(parser.options.length))))
 
-    # log.debug("Trying: " + name)
+    p = Pool(10)
+    for e in PermutationIterator(ascii_lowercase, int(parser.options.length)):
+        domain_name = str("".join(e) + parser.options.tld)
+        result = None
 
-    p = Process(target=run_name, args=(name,))
-    p.start()
-    p.join()
+        log.debug("Trying: " + domain_name)
+
+        p.apply(run_name, args=(domain_name,))
